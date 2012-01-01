06b6f73c8889cbfe5f54121d31fa896d6ed0f0c1
augus790302@gmail.com <augus790302@gmail.com@d7268285-1c95-b2eb-e02e-42e2cee31834>
2012-01-01 16:13:02 +0000

    FunctionPoint完成,但尚未測試

---
+++ trunk/src/com/projectplus/estimation/FunctionPointsDataStructure.java
@@ -0,0 +1,25 @@
+package com.projectplus.estimation;
+
+public class FunctionPointsDataStructure {
+	int UDF;
+	double VAF;
+	int FP;
+	public int getUDF() {
+		return UDF;
+	}
+	public void setUDF(int uDF) {
+		UDF = uDF;
+	}
+	public double getVAF() {
+		return VAF;
+	}
+	public void setVAF(double vAF) {
+		VAF = vAF;
+	}
+	public int getFP() {
+		return FP;
+	}
+	public void setFP(int fP) {
+		FP = fP;
+	}
+}
